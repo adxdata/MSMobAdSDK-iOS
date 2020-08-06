518fa3caa8c904be2e301b94545cb4740ec6f7eb
Lei Dai <2-meishu@users.noreply.git.adxdata.com>
2020-08-06 19:40:42 +0800

    0.8.74

--- MSMobAdSDK/enai/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ MSMobAdSDK/enai/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.73";
-static NSInteger kMSVersionCode = 873;
+static NSString *kMSVersionName = @"0.8.74";
+static NSInteger kMSVersionCode = 874;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
